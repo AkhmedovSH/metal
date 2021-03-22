f5522a31c86a28df7a28cdbca66c1c8864f91587
Shokhrukh <aiiiypuk@mail.ru>
2021-03-22 18:33:40 +0500

    mobile menu added

--- src/App.vue
+++ src/App.vue
@@ -4,36 +4,164 @@
 			<div class="d-flex">
 				<div class="header-logo-wrapper mr-4">
 					<div class="vertical-center">
-						<img src="@/assets/images/header_logo.svg" alt="logo">
+						<img src="@/assets/images/header_logo.svg" alt="logo" width="56" height="56">
 					</div>
 				</div>
 				<div class="header-navbar-nav bg-f2">
-					<ul class="header-navbar-ul">
-						<li><a href="#">Услуги</a></li>
-						<li><a href="#">Производство</a></li>
-						<li><a href="#">Проекты</a></li>
-						<li><a href="#">партнеры</a></li>
-						<li><a href="#">О компании</a></li>
-						<li><a href="#">Новости</a></li>
-						<li><a href="#">контакты</a></li>
+					<ul class="header-navbar-ul h-100">
+						<li class="vertical-center header-navbar-ul-li">
+							<a class="parent-link" href="#">Услуги</a>
+							<ul class="child-ul child-ul-big">
+								<li><a class="child-link" href="#">Конструкторское бюро</a></li>
+								<li><a class="child-link" href="#">Задание на проектирование</a></li>
+							</ul>
+						</li>
+						<li class="vertical-center header-navbar-ul-li">
+							<a class="parent-link" href="#">Производство</a>
+							<ul class="child-ul child-ul-big">
+								<li><a class="child-link" href="#">Уникальные здания</a></li>
+								<li><a class="child-link" href="#">Мархи</a></li>
+								<li><a class="child-link" href="#">Ангары</a></li>
+								<hr class="my-1">
+								<li><a class="ml-3 child-link-inner" href="#">Арочные ангары</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Прямостенные ангары</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Шатровые ангары</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Полигональные ангары</a></li>
+								<li><a class="child-link" href="#">Промышленные здания</a></li>
+								<li><a class="child-link" href="#">Строительные краны</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Козловые краны</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Мостовые краны</a></li>
+								<li><a class="child-link" href="#">Металлопрокат</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Двутавр</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Тавр</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Швеллер</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Уголок</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Листовой материал</a></li>
+								<li><a class="ml-3 child-link-inner" href="#">Гофрированная балка</a></li>
+							</ul>
+						</li>
+						<li class="vertical-center"><a class="parent-link" href="#">Партнеры</a></li>
+						<li class="vertical-center"><a class="parent-link" href="#">Проекты</a></li>
+						<li class="vertical-center header-navbar-ul-li">
+							<a class="parent-link" href="#">О нас</a>
+							<ul class="child-ul child-ul-big">
+								<li><a class="child-link" href="#">О компании</a></li>
+								<li><a class="child-link" href="#">Производтвенный комплекс</a></li>
+							</ul>
+						</li>
+						<li class="vertical-center"><a class="parent-link" href="#">Новости</a></li>
+						<li class="vertical-center"><a class="parent-link" href="#">Контакты</a></li>
 					</ul>
 					<div class="d-flex h-100">
+						<div class="header-toggle-menu" @click="showMenu = !showMenu">
+							<div class="d-flex justify-content-center h-100">
+								<div class="vertical-center">
+									<img src="@/assets/images/general/menu_icon.png" alt="icon_search" width="32">
+								</div>
+							</div>
+						</div>
 						<div class="d-flex justify-content-center header-search-block">
 							<div class="vertical-center ">
 								<img src="@/assets/images/Icon_search.svg" alt="icon_search" width="32">
 							</div>
 						</div>
 						<div class="header-recall-btn cursor-pointer px-5">
-							<div class="color-fb">Заказать звонок</div>
+							<div class="color-fb text-center">Заказать звонок</div>
 						</div>
+					</div>
+				</div>
+			</div>
+			<div class="mobile-menu" :style="{ display:showMenu ? 'block' : 'none' }">
+				<div class="mobile-menu-wrapper">
+					<div class="p-3">
+						<div class="d-flex">
+							<div class="w-50">
+								<div class="parent-link">
+									О нас
+								</div>
+								<ul class="child-ul">
+									<li><a class="child-link" href="#">О компании</a></li>
+									<li><a class="child-link" href="#">Производтвенный <br> комплекс</a></li>
+								</ul>
+								<div class="parent-link">
+									Услуги
+								</div>
+								<ul class="child-ul">
+									<li><a class="child-link" href="#">Конструкторское бюро</a></li>
+									<li><a class="child-link" href="#">Задание на  <br> проектирование</a></li>
+								</ul>
+								<div class="py-2">
+									<a class="parent-link" href="#">Партнеры</a>
+								</div>
+								<div class="py-2">
+								<a class="parent-link" href="#">Проекты</a>
+								</div>
+								<div class="py-2">
+									<a class="parent-link" href="#">Новости</a>
+								</div>
+								<div class="py-2">
+									<a class="parent-link" href="#">контакты</a>
+								</div>
+								<p class="position-absolute m-0 mobile-menu-info">
+									По вопросам партнерства <br>
+									+998 71 203 00 00 <br>
+									info@akfagroup.com
+								</p>
+							</div>
+							<div class="w-50">
+								<a class="parent-link" href="">Производство</a>
+								<ul class="child-ul">
+									<li><a class="child-link" href="#">Уникальные здания</a></li>
+									<li><a class="child-link" href="#">Мархи</a></li>
+									<li>
+										<a class="child-link" href="#">Ангары</a>
+										<hr class="my-1">
+										<ul class="child-ul">
+											<li><a href="#" class="ml-3 child-link-inner">Арочные ангары</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Прямостенные ангары</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Шатровые ангары</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Полигональные ангары</a></li>
+										</ul>
+									</li>
+								</ul>
+								
+								<ul class="child-ul">
+									<li><a href="#" class="child-link">Промышленные здания</a></li>
+									<li>
+										<a href="#" class="child-link">Строительные краны</a>
+										<ul class="child-ul">
+											<li><a href="#" class="ml-3 child-link-inner">Козловые краны</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Мостовые краны</a></li>
+										</ul>
+									</li>
+								</ul>
+
+								<ul class="child-ul">
+									<li>
+										<a href="#" class="child-link">Металлопрокат</a>
+										<ul class="child-ul">
+											<li><a href="#" class="ml-3 child-link-inner">Двутавр</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Тавр</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Швеллер</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Уголок</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Листовой материал</a></li>
+											<li><a href="#" class="ml-3 child-link-inner">Гофрированная балка</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+					</div>
+					<hr>
+					<div class="p-3">
+						la la la
+					</div>
 					</div>
 				</div>
 			</div>
 		</header>
 
 		<div class="bg-video-wrap">
-			<video src="promo.mp4" loop muted autoplay>
-			</video>
+			<video src="promo.mp4" loop muted autoplay></video>
 			<div class="overlay"></div>
 			<div class="content-over-video">
 				<div class="content-over-video-image">
@@ -628,6 +756,7 @@ export default {
 	components: { VueSlickCarousel },
 	data() {
 		return {
+			showMenu: false,
 			productionTypes: {
 				dots: false,
 				arrows: false,
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import middlewarePipeline from "./middlewarePipeline";
+
+Vue.use(Router);
+const router = new Router({
+  mode: "hash",
+  routes: [
+    {
+      path: "/",
+      redirect: "/home",
+      component: () => import("@/layouts/main/Main.vue"),
+      children: [
+        {
+          path: "/home",
+          name: "home",
+          component: () => import("@/views/Home.vue"),
+        },
+        {
+          path: "/modules",
+          name: "modules",
+          component: () => import("@/views/modules/Index.vue"),
+        },
+      ]
+    }
+    // Redirect to 404 page, if no match found
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.middleware) {
+    return next();
+	}
+
+  const middleware = to.meta.middleware;
+
+  const context = {to, from, next};
+
+  return middleware[0]({
+    ...context,
+    next: middlewarePipeline(context, middleware, 1)
+  });
+});
+
+export default router;
