30cb19897a3db4b020b8cbbd06b2eafdd587caf3
Shokhrukh <aiiiypuk@mail.ru>
2021-04-05 17:54:58 +0500

    done

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="mt96">
-		<div class="page-px py-5">
+		<div class="page-px py-5 bg-2">
 			<div class="section-breadcrumbs">
 				О нас <span class="mx-3">/</span> <strong>О компании</strong>
 			</div>
@@ -9,9 +9,22 @@
 		<div class="position-relative">
 			<div class="about-img-overlay"></div>
 			<img src="@/assets/images/about/about_bg.png">
+			<div class="position-absolute page-px about-img-content">
+				<div class="media mb-5">
+					<img src="@/assets/images/red_element.svg" alt="logo" class="mr-5" height="96">
+					<div class="media-body">
+						<h1>О компании</h1>
+					</div>
+				</div>
+				<p class="color-2">
+					Компания Avant Metal Group зарекомендовала себя как надежного и <br>
+					ответственного производителя, в продукции которого вы можете <br>
+					быть уверены. 
+				</p>
+			</div>
 		</div>
 
-		<div class="page-px py-5">
+		<div class="page-px py-5 bg-2">
 			<div class="row">
 				<div class="col-md-6">
 					<hr>
@@ -37,13 +50,13 @@
 			<img src="@/assets/images/about/about_bg2.png">
 		</div>
 
-		<div class="page-px py-5">
+		<div class="page-px pt-5 bg-2">
 			<div class="row">
 				<div class="col-md-6 mb-5">
 					<div class="media">
 						<img src="@/assets/images/red_element.svg" alt="logo" class="mr-5" height="96">
 						<div class="media-body">
-							<p class="color-01"><span class="ml-4"></span> Производственный комплекс компании позволяет предложить такие виды услуг как строительство сооружений
+							<p class="color-1"><span class="ml-4"></span> Производственный комплекс компании позволяет предложить такие виды услуг как строительство сооружений
 							любой сложности из ЧМК и ЛСТК, возведение сооружений из систем Мархи, а также широкий спектр продуктов и услуг применяемых в 
 							строительстве больших высотных зданий.</p>
 						</div>
@@ -54,7 +67,7 @@
 					<div class="media">
 						<img src="@/assets/images/red_element.svg" alt="logo" class="mr-5" height="96">
 						<div class="media-body">
-							<p class="color-01"><span class="ml-4"></span> Наши высоквалифицированные специалисты помогут Вам в выборе наилучшего конструктивного решения,
+							<p class="color-1"><span class="ml-4"></span> Наши высоквалифицированные специалисты помогут Вам в выборе наилучшего конструктивного решения,
 							 разработать КМ, КМД, КЖ, и подобрать мматериалы для Вашего будущего объекта с учетом технико-экономических факторов.</p>
 						</div>
 					</div>
@@ -71,24 +84,135 @@
 					<img src="@/assets/images/about/line.png">
 				</div>
 				<h4>Главный двигатель нашей компании – высокий уровень мотивации </h4>
-				<p class="color-01">Мы всегда открыты для сотрудничества и готовы помочь в решении Ваших задач.</p>
+				<p class="color-1">Мы всегда открыты для сотрудничества и готовы помочь в решении Ваших задач.</p>
 			</div>
 
 			<hr class="my-5">
 
-			<div class="row">
+			<div class="row mb-5">
 				<div class="col-md-6">
 					<div class="media">
 						<img src="@/assets/images/red_element.svg" alt="logo" class="mr-5" height="96">
 						<div class="media-body">
-							<p class="color-01"><span class="ml-4"></span> Avant Metal Group ориентирована на предоставление
+							<p class="color-1"><span class="ml-4"></span> Avant Metal Group ориентирована на предоставление
 							 высокотехнологичных и доступных решений в области металлоконструкций и стройматериалов.</p>
 						</div>
 					</div>
 				</div>
 			</div>
+
+			<div class="row">
+				<div class="col-md-2 mb-5">
+					<div class="bg-1 py-5 icons-block">
+						<div class="d-flex justify-content-center mb-3">
+							<img src="@/assets/images/about/about_icon_1.svg">
+						</div>
+						<p class="text-center">Уникальные <br>здания</p>
+					</div>
+				</div>
+				<div class="col-md-2 mb-5">
+					<div class="bg-1 py-5 icons-block">
+						<div class="d-flex justify-content-center mb-3">
+							<img src="@/assets/images/about/about_icon_2.svg">
+						</div>
+						<p class="text-center">Мархи</p>
+					</div>
+				</div>
+				<div class="col-md-2 mb-5">
+					<div class="bg-1 py-5 icons-block">
+						<div class="d-flex justify-content-center mb-3">
+							<img src="@/assets/images/about/about_icon_3.svg">
+						</div>
+						<p class="text-center">Ангары</p>
+					</div>
+				</div>
+				<div class="col-md-2 mb-5">
+					<div class="bg-1 py-5 icons-block">
+						<div class="d-flex justify-content-center mb-3">
+							<img src="@/assets/images/about/about_icon_4.svg">
+						</div>
+						<p class="text-center">Промышленные <br>здания</p>
+					</div>
+				</div>
+				<div class="col-md-2 mb-5">
+					<div class="bg-1 py-5 icons-block">
+						<div class="d-flex justify-content-center mb-3">
+							<img src="@/assets/images/about/about_icon_5.svg">
+						</div>
+						<p class="text-center">Строительные <br>краны</p>
+					</div>
+				</div>
+				<div class="col-md-2 mb-5">
+					<div class="bg-1 py-5 icons-block">
+						<div class="d-flex justify-content-center mb-3">
+							<img src="@/assets/images/about/about_icon_6.svg">
+						</div>
+						<p class="text-center">Металлопрокат</p>
+					</div>
+				</div>
+			</div>
 		</div>
 
+		<div class="page-px py-5 bg-1">
+			<div class="section-breadcrumbs">
+				Производство <span class="mx-3">/</span> <strong>Реализованные проекты</strong>
+			</div>
+		</div>
+
+		<VueSlickCarousel v-bind="slider3">
+			<div class="container-fluid p-0">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="slider3" :style="{'background-image': 'url(' + require('@/assets/images/about/about_slider_1.png') + ')'}">
+							<div class="slider-2-overlay"></div>
+							<div class=" slider2-content">
+								<div class="vertical-center h-100">
+									<div class="d-flex">
+										<img src="@/assets/images/slider1/ico1.svg" alt="logo" class="mr-5">
+										<div>
+											<h2>Торговый комплекс Emporia</h2>
+											<p>г.Ташкент. Шайхантахурский район.</p>
+											<hr class="mb-3">
+											<p>
+												Композиция из бронзовой охры была вдохновлена образами Пантеона. <br>
+												Выгнутая стеклянная поверхность охватывает диагональный разрез, <br>
+												пронизывающий здание.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</VueSlickCarousel>
+
+		<news></news>
 
 	</div>
 </template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import '@/assets/css/vue-slick.css'
+import '@/assets/css/vue-slick-theme.css'
+import News from '@/components/News.vue'
+export default {
+  name: 'Home',
+	components: { VueSlickCarousel, News },
+	data() {
+		return {
+			slider3: {
+				dots: false,
+				arrows: true,
+				infinite: true,
+				speed: 500,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				initialSlide: 0,
+			},
+		}
+	}
+}
+</script>
